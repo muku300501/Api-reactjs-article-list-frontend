90ca64e19d28d3d24e84413e046d59a17ff8f2a5
Mukund <mukund.sc01@gmail.com>
2021-03-02 20:29:32 +0530

    first commit

---
+++ src/APIService.js
@@ -0,0 +1,46 @@
+export default class APIService {
+    static UpdateArticle(article_id, body, token) {
+        return fetch(`http://127.0.0.1:8000/api/articles/${article_id}/`, {
+            'method': 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`,
+            },
+            body: JSON.stringify(body),
+        })
+        .then(response => response.json())
+    } 
+
+    static InsertArticle(body, token) {
+        return fetch(`http://127.0.0.1:8000/api/articles/`, {
+            'method': 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`,
+            },
+            body: JSON.stringify(body),
+        })
+        .then(response => response.json())
+    }
+
+    static DeleteArticle(article_id, token) {
+        return fetch(`http://127.0.0.1:8000/api/articles/${article_id}/`, {
+            'method': 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`,
+            } 
+        })
+    }
+
+    static LoginUser(body) {
+        return fetch(`http://127.0.0.1:8000/auth/`, {
+            'method': 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        })
+        .then(response => response.json())
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react'; 
+import ArticleList from './Components/ArticleList'; 
+import Form from './Components/Form';
+import {useCookies} from 'react-cookie';
+
 
 function App() {
+
+  const [articles, setArticles] = useState([])
+  const [editArticle, setEditArticle] = useState(null)
+  const [token] = useCookies(['mytoken'])
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/articles/', {
+      'method': 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token['mytoken']}`
+      }
+    })
+    .then(response => response.json())
+    .then(response => setArticles(response))
+    .catch(error => console.log(error))
+  
+  }, [token])
+
+  const editBtn = (article) => {
+     setEditArticle(article)
+  }
+
+  const updateInformation = (article) => {
+    const new_article = articles.map(myarticle => {
+      if(myarticle.id === article.id){
+        return article;
+      }
+      else{
+        return myarticle;
+      }
+    })
+
+    setArticles(new_article)
+  } 
+
+  const articleForm = () => {
+    setEditArticle({title:'', description:''})
+  }
+
+  const insertedInformation = (article) => {
+    const new_articles = [...articles, article]
+    setArticles(new_articles)
+  }
+
+  const deleteBtn = (article) => {
+    const new_articles = articles.filter(myarticle => {
+      if(myarticle.id === article.id) {
+        return false;
+      }
+      return true;
+    })
+
+    setArticles(new_articles)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col">
+          <h1>Django And ReactJS Course</h1>
+          <br />
+        </div>
+        <div className="col">
+          <button onClick={articleForm} className="btn btn-primary">Insert Article</button>
+        </div>
+      </div>
+      <br />
+      <ArticleList articles={articles} editBtn={editBtn} deleteBtn={deleteBtn} />
+
+      {editArticle ? <Form article={editArticle} updateInformation={updateInformation} insertedInformation={insertedInformation} /> : null }
+
     </div>
   );
 }
